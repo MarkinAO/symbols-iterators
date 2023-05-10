7804994de51a93dcae733732204754ac4a7c4e5e
MarkinAO <habar77@yandex.ru>
2023-05-10 20:54:28 +0700

    init commit

---
+++ src/js/Team.js
@@ -0,0 +1,24 @@
+export default class Team {
+  constructor(...chars) {
+    this.team = chars;
+  }
+
+  [Symbol.iterator]() {
+    const { team } = this;
+    let index = 0;
+    return {
+      next() {
+        if (index === team.length) {
+          return {
+            value: undefined,
+            done: true,
+          };
+        }
+        return {
+          value: team[index++],
+          done: false,
+        };
+      },
+    };
+  }
+}
